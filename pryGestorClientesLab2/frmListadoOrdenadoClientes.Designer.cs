63a104945ecd308d5585ec6db7d6ae7d18f07d20
Leandro <Gaidoleandro10@gmail.com>
2026-05-28 23:39:23 -0300

    continuando

--- pryGestorClientesLab2/frmListadoOrdenadoClientes.Designer.cs
+++ pryGestorClientesLab2/frmListadoOrdenadoClientes.Designer.cs
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCampo = new System.Windows.Forms.Label();
+            this.cmbModo = new System.Windows.Forms.ComboBox();
+            this.cmbCampo = new System.Windows.Forms.ComboBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.dgvListar = new System.Windows.Forms.DataGridView();
+            this.clCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clLimite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clDeuda = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(404, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 18);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Modo";
+            // 
+            // lblCampo
+            // 
+            this.lblCampo.AutoSize = true;
+            this.lblCampo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCampo.Location = new System.Drawing.Point(19, 79);
+            this.lblCampo.Name = "lblCampo";
+            this.lblCampo.Size = new System.Drawing.Size(57, 18);
+            this.lblCampo.TabIndex = 16;
+            this.lblCampo.Text = "Campo";
+            // 
+            // cmbModo
+            // 
+            this.cmbModo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbModo.FormattingEnabled = true;
+            this.cmbModo.Items.AddRange(new object[] {
+            "Ascendente",
+            "Descendente"});
+            this.cmbModo.Location = new System.Drawing.Point(493, 76);
+            this.cmbModo.Name = "cmbModo";
+            this.cmbModo.Size = new System.Drawing.Size(198, 26);
+            this.cmbModo.TabIndex = 15;
+            // 
+            // cmbCampo
+            // 
+            this.cmbCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCampo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCampo.FormattingEnabled = true;
+            this.cmbCampo.Items.AddRange(new object[] {
+            "Código",
+            "Límite",
+            "Deuda"});
+            this.cmbCampo.Location = new System.Drawing.Point(92, 76);
+            this.cmbCampo.Name = "cmbCampo";
+            this.cmbCampo.Size = new System.Drawing.Size(198, 26);
+            this.cmbCampo.TabIndex = 14;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.Location = new System.Drawing.Point(632, 140);
+            this.btnListar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(149, 34);
+            this.btnListar.TabIndex = 13;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(9, 10);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(363, 18);
+            this.lblTitulo.TabIndex = 12;
+            this.lblTitulo.Text = "Seleccione como desea ordenar el listado de clientes:";
+            // 
+            // dgvListar
+            // 
+            this.dgvListar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clCodigo,
+            this.clUsuario,
+            this.clLimite,
+            this.clDeuda});
+            this.dgvListar.Location = new System.Drawing.Point(12, 190);
+            this.dgvListar.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvListar.Name = "dgvListar";
+            this.dgvListar.RowHeadersWidth = 51;
+            this.dgvListar.Size = new System.Drawing.Size(769, 274);
+            this.dgvListar.TabIndex = 11;
+            // 
+            // clCodigo
+            // 
+            this.clCodigo.HeaderText = "Código";
+            this.clCodigo.MinimumWidth = 6;
+            this.clCodigo.Name = "clCodigo";
+            this.clCodigo.Width = 120;
+            // 
+            // clUsuario
+            // 
+            this.clUsuario.HeaderText = "Usuario";
+            this.clUsuario.MinimumWidth = 6;
+            this.clUsuario.Name = "clUsuario";
+            this.clUsuario.Width = 200;
+            // 
+            // clLimite
+            // 
+            this.clLimite.HeaderText = "Limite";
+            this.clLimite.MinimumWidth = 6;
+            this.clLimite.Name = "clLimite";
+            this.clLimite.Width = 200;
+            // 
+            // clDeuda
+            // 
+            this.clDeuda.HeaderText = "Deuda";
+            this.clDeuda.MinimumWidth = 6;
+            this.clDeuda.Name = "clDeuda";
+            this.clDeuda.Width = 200;
+            // 
+            // frmListadoOrdenadoClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 471);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCampo);
+            this.Controls.Add(this.cmbModo);
+            this.Controls.Add(this.cmbCampo);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.dgvListar);
+            this.Name = "frmListadoOrdenadoClientes";
             this.Text = "frmListadoOrdenadoClientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCampo;
+        private System.Windows.Forms.ComboBox cmbModo;
+        private System.Windows.Forms.ComboBox cmbCampo;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.DataGridView dgvListar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clUsuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clLimite;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clDeuda;
     }
 }
